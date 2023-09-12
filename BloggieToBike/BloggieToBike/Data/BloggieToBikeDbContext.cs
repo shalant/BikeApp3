38aec6bb668ea781bf272cb022951f64d1975d57
shalant <doug.rosenberg@gmail.com>
2023-09-11 21:41:08 -0500

    took branch movies2 from BloggieToBike2 and pasted it here

---
+++ BloggieToBike/BloggieToBike/Data/BloggieToBikeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BloggieToBike.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using BloggieToBike.Models;
+
+namespace BloggieToBike.Web.Data
+{
+    public class BloggieToBikeDbContext : DbContext
+    {
+        public BloggieToBikeDbContext(DbContextOptions<BloggieToBikeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BikeRoute> BikeRoutes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BikeRouteLike> BikeRouteLikes { get; set; }
+        public DbSet<BikeRouteComment> BikeRouteComments { get; set; }
+        public DbSet<BloggieToBike.Models.NewBikeRoute> NewBikeRoute { get; set; } = default!;
+    }
+}
---
+++ BloggieToBike/BloggieToBike/Pages/BikeRoutes/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BloggieToBike.Web.Models.Domain;
+
+namespace BloggieToBike.Pages.BikeRoutes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Web.Data.BloggieToBikeDbContext _context;
+
+        public IndexModel(Web.Data.BloggieToBikeDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<BikeRoute> BikeRoute { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Directions { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BikeRouteDirection { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            //use LINQ to get a list of directions
+            IQueryable<string> directionQuery = from d in _context.BikeRoutes
+                                                orderby d.Direction
+                                                select d.Direction;
+
+            var bikeRoutes = from b in _context.BikeRoutes
+                             select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                bikeRoutes = bikeRoutes.Where(s => s.Name.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BikeRouteDirection))
+            {
+                bikeRoutes = bikeRoutes.Where(d => d.Direction == BikeRouteDirection);
+            }
+
+            Directions = new SelectList(await directionQuery.Distinct().ToListAsync());
+            BikeRoute = await bikeRoutes.ToListAsync();
+
+            if (_context.BikeRoutes != null)
+            {
+                BikeRoute = await _context.BikeRoutes.ToListAsync();
+            }
+        }
+    }
+}
